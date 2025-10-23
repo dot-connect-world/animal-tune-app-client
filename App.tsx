9e8af4c7312a30533ce7ff5c1d30861515407e10
kchranger <kchranger@naver.com>
2025-10-23 23:02:41 +0900

    chore: 디버그 팝업 메시지 숨김 처리

--- App.tsx
+++ App.tsx
@@ -3,6 +3,10 @@ import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import TabNavigator from './src/navigation/TabNavigator';
 
+// Debug 메시지 비활성화
+import { LogBox } from 'react-native';
+LogBox.ignoreAllLogs();
+
 export default function App() {
   return (
     <NavigationContainer>
