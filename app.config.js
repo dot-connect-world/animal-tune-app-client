91cb3b55f30fc315241baa29ede787efab2f5482
kchranger <kchranger@naver.com>
2025-12-01 22:07:02 +0900

    chore: 배포
    - version 1.0.9
    - ios 11
    - android 10

--- app.config.js
+++ app.config.js
@@ -27,9 +27,9 @@ console.log('  admobIosBannerId:', admobIosBannerId);
 console.log('  admobAndroidBannerId:', admobAndroidBannerId);
 
 
-  const expoVersion = '1.0.8';
-  const iosVersion = '10';
-  const androidVersion = 9;
+  const expoVersion = '1.0.9';
+  const iosVersion = '11';
+  const androidVersion = 10;
 
 
 module.exports = {
